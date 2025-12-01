f2afbba68db8e2fe71f356eb5893e93c6608d09c
indkshitij <ind.kshitijsingh@gmail.com>
2025-12-01 23:41:49 +0530

    fix: carousel

--- frontend/molecules/SkillsCarousel.tsx
+++ frontend/molecules/SkillsCarousel.tsx
@@ -14,7 +14,7 @@ const SkillsCarousel = () => {
           description="A continuously evolving collection of modern tools I use to create meaningful digital experiences."
         />
 
-        <div className="-space-y-16 sm:-space-y-8 relative -mt-8">
+        <div className="-space-y-8 relative -mt-8">
           <InfiniteScroller direction="right" />
           <InfiniteScroller direction="left" />
         </div>
